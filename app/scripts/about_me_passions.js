9594a7dbf4011b913b0444981f123bb4f5f71f6f
D'Auria <sdauria@06LM-SDAURIA1.local>
2017-06-13 15:36:21 +0100

    v.0.7 - About you with counter

--- app/scripts/about_me_passions.js
+++ app/scripts/about_me_passions.js
@@ -1,13 +1,45 @@
-function AboutMePassions(txt){
+/** ============ AboutMePassions 
+ * One of passion in #about_me section has been clicked
+ * @txt -> the text to show as feedback
+ * changhing the html text of #feedback_box with the @txt in input
+*/
+function AboutMePassions(obj){
     var feedbackBox = document.getElementById("feedback_box");
     var elClone = feedbackBox.cloneNode(true);
     var parent = feedbackBox.parentNode;
-    elClone.innerHTML = txt;
+    elClone.innerHTML = obj.description;
     
     parent.replaceChild(elClone, feedbackBox);
+    
+}
 
-    /*
-    feedbackBox.classList.remove("mast__text");
-    feedbackBox.classList.add("class", "mast__text");*/
+/** ============ AboutYouPassions 
+ * One of passion in #about_you section has been clicked
+ * obj -> the text to show as feedback
+ * changhing the html text of #feedback_box with the @txt in input
+*/
+function AboutYouPassions(obj){
+    
+    var elem = document.getElementById("yourPassion_" + obj.id);
+    var txtBox = document.getElementById("feedbackAboutYou_box");
+    var txt = txtBox.innerHTML;
+    var nPassions = Number(txt.match( /\d/g));
+    console.log("Before: " + nPassions);
+    
+    if(elem.classList.contains("selected")){
+        /* Passion already selected */
+        elem.classList.remove("selected");
+        nPassions ++;
+        txt = txt.replace( /\d/g, nPassions);
+        }
+    else{
+        /* Passion already selected */
+        elem.classList.add("selected");
+        nPassions --;
+        txt = txt.replace( /\d/g, nPassions);
+    }
+    
+    txtBox.innerHTML = txt;
+    console.log("After: " + nPassions);
     
 }
--- app/scripts/generic.js
+++ app/scripts/generic.js
@@ -1,3 +1,10 @@
+/*------ Loading Bar ---------*/
+
+$(window).load(function() {
+    $('#page').css('opacity', 1);
+})
+
+/* ------ clicking on obout you -----*/
 $("#about_you_btn").click(function() {	
 	      	      
  $('#about_you').css('opacity', 1);
