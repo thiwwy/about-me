e73174630b71dce9aaf549a434e442ac846e73eb
D'Auria <sdauria@06LM-SDAURIA1.local>
2017-06-14 13:29:46 +0100

    v.0.7 -> Game almost done

--- app/scripts/about_me_passions.js
+++ app/scripts/about_me_passions.js
@@ -1,3 +1,5 @@
+var listPassionsSelected = [];
+
 /** ============ AboutMePassions 
  * One of passion in #about_me section has been clicked
  * @txt -> the text to show as feedback
@@ -21,25 +23,106 @@ function AboutMePassions(obj){
 function AboutYouPassions(obj){
     
     var elem = document.getElementById("yourPassion_" + obj.id);
+    var idSelected = "selected_" + obj.id;
+    
+    if(elem.classList.contains("selected"))
+
+        removePassionFromSelected(idSelected);
+        
+    else
+            
+        addPassionInSelected(obj);
+
+}
+
+/** === Removing a Passion From the Selected Box === 
+    * @id => id of the selected element in the Selected Box 
+*/
+function removePassionFromSelected(id){
+
+    /** === Taking the respectively element in the Passion Box === */
+    var idObj = id.substr((id.indexOf('_')+1), id.length);
+    var idPassion = "yourPassion_" + idObj;
+    var passionToUnselect = document.getElementById(idPassion);
+
+    /** === Unselect the element and remove it from the Selected Box === */
+    passionToUnselect.classList.remove("selected");
+    document.getElementById(id).remove();
+
+    /** Updating the Counter */
     var txtBox = document.getElementById("feedbackAboutYou_box");
     var txt = txtBox.innerHTML;
     var nPassions = Number(txt.match( /\d/g));
-    console.log("Before: " + nPassions);
-    
-    if(elem.classList.contains("selected")){
-        /* Passion already selected */
-        elem.classList.remove("selected");
-        nPassions ++;
-        txt = txt.replace( /\d/g, nPassions);
-        }
-    else{
-        /* Passion already selected */
-        elem.classList.add("selected");
-        nPassions --;
-        txt = txt.replace( /\d/g, nPassions);
-    }
-    
+    nPassions ++;
+    txt = txt.replace( /\d/g, nPassions);
     txtBox.innerHTML = txt;
-    console.log("After: " + nPassions);
+
+    removeFromList(idObj);
+
+}
+
+/** === Adding the Passion in the Selected Box === 
+    * @id => id of the selected element to add in the Selected Box
+*/
+function addPassionInSelected(obj){
+
+    var id = "selected_" + obj.id;
+
+    /** Checking The counter */
+    var txtBox = document.getElementById("feedbackAboutYou_box");
+    var txt = txtBox.innerHTML;
+    var nPassions = Number(txt.match( /\d/g));
     
+    if(nPassions == 0)
+                return;
+
+    /** === Taking the respectively element in the Passion Box and select it === */
+    var idPassion = "yourPassion_" + id.substr((id.indexOf('_')+1), id.length);
+    var passionToSelect = document.getElementById(idPassion);
+    passionToSelect.classList.add("selected");
+
+    /** Creating new element to add in the webpage */
+    var parentToAdd = document.createElement("div");
+    parentToAdd.setAttribute('id', id);
+    parentToAdd.setAttribute('class','mdl-cell mdl-cell--4-col addedInBox');
+    parentToAdd.setAttribute('onclick', "removePassionFromSelected('" + id + "')");
+
+     /** Cloning the element selected */
+    var cloneSelected = passionToSelect.firstChild.cloneNode(true);
+    cloneSelected.removeAttribute('class');
+    cloneSelected.removeAttribute('id');
+    parentToAdd.appendChild(cloneSelected);
+
+    /** Adding the Element in the page */
+    var selectedBox = document.getElementById("PassionsSelected");  
+    selectedBox.appendChild(parentToAdd);
+
+    /** Updating the Counter */
+    nPassions --;
+    txt = txt.replace( /\d/g, nPassions);
+    txtBox.innerHTML = txt;
+
+    listPassionsSelected.push(obj);
+
+    if(nPassions == 0)
+                matchFeedback();
+
+}
+
+function matchFeedback(){
+    var totMatch = 0;
+
+    for(var i=0; i<listPassionsSelected.length ; i++)
+        totMatch += parseInt(listPassionsSelected[i].match);
+
+}
+
+function removeFromList(id){
+
+    for(var i=0; i<listPassionsSelected.length; i++){
+        if(listPassionsSelected[i].id === id)
+                posToCanc = i;
+    }
+
+    listPassionsSelected.splice(posToCanc, 1);
 }
--- app/scripts/generic.js
+++ app/scripts/generic.js
@@ -10,3 +10,21 @@ $("#about_you_btn").click(function() {
  $('#about_you').css('opacity', 1);
 
 });
+
+/* ------ clicking on about me -----*/
+function aboutMeReset(description, description_2 = undefined){
+
+    var feedbackBox = document.getElementById("feedback_box");
+    var elClone = feedbackBox.cloneNode(true);
+    var parent = feedbackBox.parentNode;
+    elClone.innerHTML = description;
+
+    if(description_2 != undefined){
+        var br = document.createElement("br");
+        elClone.appendChild(br);
+        elClone.innerHTML += description_2;
+    }
+    
+    parent.replaceChild(elClone, feedbackBox);
+
+}
